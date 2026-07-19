c6c7fad737ba7661e47cd65933acc8e58e2b3e60
yanbin <yanbinx@gmail.com>
2026-07-19 21:43:24 +0800

    Remove legacy device layer stubs

--- device/device.go
+++
@@ -1,6 +0,0 @@
-package device
-
-// The legacy camera, lock, 4G, and vending-machine control layer has been
-// removed from the active agent build. New device-facing behavior should be
-// added through focused services such as serial_bridge or the future A/B TCP
-// interfaces.
